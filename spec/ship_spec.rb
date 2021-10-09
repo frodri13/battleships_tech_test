1bedd86905cc64124be558bb09697bb7fbaaa348
Fabio <alessf13@gmail.com>
2021-10-09 08:01:26 +0200

    test: medium and large size tests created

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe Ship do
 
 		expect(ship.size).to eq("s")
 	end
+
+	it 'can be a medium ship' do
+		ship = Ship.new("m")
+
+		expect(ship.size).to eq("m")
+	end
+
+	it 'can be a large ship' do
+		ship = Ship.new("l")
+
+		expect(ship.size).to eq("l")
+	end
 end

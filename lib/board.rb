ba5616f4e0f4281470c6d219c17ba333a58f3692
Fabio <alessf13@gmail.com>
2021-10-09 08:22:29 +0200

    feat: each column has 10 rows

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,6 @@ class Board
 	attr_reader :grid
 
 	def initialize
-		@grid = Array.new(10)
+		@grid = Array.new(10) {Array.new(10)}
 	end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Board do
 	 it 'has 10 columns' do
 		 expect(subject.grid.length).to eq(10)
 	 end
+
+	 it 'has 10 rows per column' do
+		 expect(subject.grid[4].length).to eq(10)
+	 end
 end
